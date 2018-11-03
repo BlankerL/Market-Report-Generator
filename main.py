2c86e05a89f7c1506fbfb3df370e6cb925967e44
me@leolin.cn <Ljb87887060!>
2018-11-03 15:10:33 +0800

    Make the codes more readable.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import datetime
 import time
 import sys
-import Chinese_Index_Overview
+import Index_Overview
 import spider_Findtoday
 
 
@@ -18,15 +18,15 @@ if __name__ == "__main__":
     print('全球市场')
     Stock_ID_CN = '000001.SH,399001.SZ,399006.SZ'
     Stock_ID_List_CN = Stock_ID_CN.split(',')
-    Chinese_Index_Overview.market_overview(Stock_ID_List_CN, DATE)
+    Index_Overview.market_overview(Stock_ID_List_CN, DATE)
     Stock_ID_Asia = "N225.GI,KS11.GI,AS51.GI"
     Stock_ID_List_Asia = Stock_ID_Asia.split(',')
-    Chinese_Index_Overview.market_overview(Stock_ID_List_Asia, DATE)
+    Index_Overview.market_overview(Stock_ID_List_Asia, DATE)
 
     print('\n成交量')
     Stock_ID_CN = '000001.SH,399001.SZ,399006.SZ'
     Stock_ID_List_CN = Stock_ID_CN.split(',')
-    Chinese_Index_Overview.volume_detector(Stock_ID_List_CN, DATE)
+    Index_Overview.volume_detector(Stock_ID_List_CN, DATE)
 
     print('\n宏观策略')
     spider_Findtoday.get_comment(date=DATE)

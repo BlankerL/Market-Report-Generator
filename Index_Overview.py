0901c4261de1832b44e5575c3883f9343ac276a5
Blanker <me@leolin.cn>
2018-11-10 21:31:07 +0800

    New API: get instant price data.

--- Index_Overview.py
+++ Index_Overview.py
@@ -9,15 +9,21 @@ class StockDetailFinder:
     def __init__(self, stock_id, date_input):
         self.stock_id = stock_id
         self.date_input = date_input  # Need to be an str
-    # TODO: If I need to get instant data, I need to use WSQ
+
     def data_manage(self):
-        get_data = w.wss(self.stock_id,
-                         "SEC_NAME,WINDCODE,CHG,PCT_CHG,CLOSE3,AMT",
+        get_name = w.wss(self.stock_id,
+                         "SEC_NAME,WINDCODE",
+                         "tradeDate=%s;priceAdj=U;cycle=D" % self.date_input)
+        get_data = w.wsq(self.stock_id,
+                         "RT_CHG,RT_PCT_CHG,rt_last,rt_amt",
                          "tradeDate=%s;priceAdj=U;cycle=D" % self.date_input)
         data_dict = {}
-        for i in range(len(get_data.Codes)):
-            codes = get_data.Codes[i]
+        for i in range(len(get_name.Codes)):
+            codes = get_name.Codes[i]
             data_dict[codes] = {}
+            for j in range(len(get_name.Fields)):
+                data_name = get_name.Fields[j]
+                data_dict[codes][data_name] = get_name.Data[j][i]
             for j in range(len(get_data.Fields)):
                 data_name = get_data.Fields[j]
                 data_dict[codes][data_name] = get_data.Data[j][i]
@@ -25,9 +31,9 @@ class StockDetailFinder:
 
     def data_printer(self):
         data_dict = self.data_manage()
-        return data_dict[self.stock_id]['SEC_NAME'], round(data_dict[self.stock_id]['PCT_CHG'], 2), \
-            round(data_dict[self.stock_id]['CHG'], 2), round(data_dict[self.stock_id]['CLOSE3'], 2), \
-            data_dict[self.stock_id]['AMT']
+        return data_dict[self.stock_id]['SEC_NAME'], round(data_dict[self.stock_id]['RT_PCT_CHG']*100, 2), \
+            round(data_dict[self.stock_id]['RT_CHG'], 2), round(data_dict[self.stock_id]['RT_LAST'], 2), \
+            data_dict[self.stock_id]['RT_AMT']
 
 
 # The main difference between China and Others are the delivery of contents.
